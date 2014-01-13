2a3c8866bf52e70b546aab955c331a244f07cc86
Dave Benjamin <daveb@twitter.com>
2014-01-13 06:22:05 -0800

    Colorize spelling corrections

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -2,6 +2,8 @@
 
 setopt correct_all
 
+export SPROMPT="zsh: correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [nyae]? "
+
 alias man='nocorrect man'
 alias mv='nocorrect mv'
 alias mysql='nocorrect mysql'
