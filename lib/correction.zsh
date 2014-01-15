635df0a89ac622285282d52fe44fd2ec18d8b4b8
Dave Benjamin <daveb@twitter.com>
2014-01-14 20:40:01 -0800

    Don't correct aurora

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -4,8 +4,9 @@ setopt correct_all
 
 export SPROMPT="zsh: correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [nyae]? "
 
+alias aurora='nocorrect aurora'
 alias man='nocorrect man'
+alias mkdir='nocorrect mkdir'
 alias mv='nocorrect mv'
 alias mysql='nocorrect mysql'
-alias mkdir='nocorrect mkdir'
 alias sudo='nocorrect sudo'
